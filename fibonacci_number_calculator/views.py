869cabe163cf6e6924c570f968e85cb13398813b
mayankgupta804 <mayank.gupta804@gmail.com>
2019-04-03 01:10:24 +0530

    Create views and templates for index and fib_calculate routes

--- fibonacci_number_calculator/views.py
+++ fibonacci_number_calculator/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .fibonacci_number import calculate_fibonacci_number
+import time
 
-# Create your views here.
+def index(request):
+    return render(request, 'fibonacci/index.html', {
+        'title': 'Enter any number'
+    })
+
+def fib_calculator(request):
+    number = request.GET.get('number', '')
+    if number == '':
+        return redirect('/')
+    start = time.time()
+    result = calculate_fibonacci_number(number)
+    stop = time.time()
+    total_time = stop - start
+    print(total_time)
+    if result == -1:
+        return render(request, 'fibonacci/error.html', {
+            'title': 'Cannot calculate fibonacci number for a negative number'
+        })
+    return render(request, 'fibonacci/results.html', {
+        'result': str(result),
+        'time_taken': str(total_time)
+    })
